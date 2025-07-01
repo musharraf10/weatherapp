9207c05831f78a45848944c16c44f54f92e3b277
Shaik Musharaf <105067219+musharraf10@users.noreply.github.com>
2025-07-01 13:54:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,11 +14,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 
 // Routes
